6ae423007f632a5576d3ee60e0ad157152c16b83
Nicolas <ntenoutasse@Nicolass-MacBook-Pro-4.local>
2017-01-10 21:42:13 +0100

    typescript in VSCode

---
+++ main.js
@@ -0,0 +1,12 @@
+var Greeter = (function () {
+    function Greeter(message) {
+        this.greeting = message;
+    }
+    Greeter.prototype.greet = function () {
+        return "Hello, " + this.greeting;
+    };
+    return Greeter;
+}());
+var greeter = new Greeter(" Liviu");
+console.log(greeter.greet());
+//# sourceMappingURL=main.js.map
